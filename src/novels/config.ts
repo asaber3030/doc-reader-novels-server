b2939dd8cf9b2f7eb1b0c1b1b89d953884071b30
Abdulrahman Saber <abdulrahmansaber120@gmail.com>
2024-09-17 21:31:18 +0300

    fix novels

--- src/novels/config.ts
+++ src/novels/config.ts
@@ -1,13 +1,13 @@
 export const novelsConfig = {
   select: {
     id: true,
+    image: true,
     title: true,
     description: true,
-    image: true,
-    userId: true,
-    url: true,
     likesCount: true,
     viewsCount: true,
+    commentsCount: true,
+    userId: true,
     categoryId: true,
     createdAt: true,
     updatedAt: true,
